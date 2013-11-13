9da7a846306a38947c1a74236539d50e17c9fefe
greglange <glange@rackspace.com>
2013-11-13 16:22:15 +0000

    Bumped version to 1.0.5

--- qork/__init__.py
+++ qork/__init__.py
@@ -13,6 +13,6 @@
 # permissions and limitations under the License
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 0, 4)
+version_info = (1, 0, 5)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
