f6636c6cf0099e06257a3e8a901c4baf95e7fb23
Greg Lange <greglange@gmail.com>
2014-02-04 14:49:58 +0000

    Bumped version to 1.0.10

--- qork/__init__.py
+++ qork/__init__.py
@@ -13,6 +13,6 @@
 # permissions and limitations under the License
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 0, 9)
+version_info = (1, 0, 10)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
